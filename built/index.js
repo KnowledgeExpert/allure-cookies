a6595152e144d2bf78ceb74c77bc9468122ac705
Alexander Popov <aleksander.popov.93@gmail.com>
2018-04-12 07:35:12 +0300

    update index

--- built/index.js
+++ built/index.js
@@ -1,7 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var allureReporterExtensions_1 = require("./allureReporterExtensions");
-exports.AllureReporterExtensions = allureReporterExtensions_1.AllureReporterExtensions;
+const allureReporterExtensions_1 = require("./allureReporterExtensions");
 var gherkin_1 = require("./gherkin");
 exports.Gherkin = gherkin_1.Gherkin;
+exports.getJasmineAllureReporter = allureReporterExtensions_1.AllureReporterExtensions.getJasmineAllureReporter;
+exports.addArgument = allureReporterExtensions_1.AllureReporterExtensions.addArgument;
+exports.addDescription = allureReporterExtensions_1.AllureReporterExtensions.addDescription;
+exports.addEnvironment = allureReporterExtensions_1.AllureReporterExtensions.addEnvironment;
+exports.attachScreenshot = allureReporterExtensions_1.AllureReporterExtensions.attachScreenshot;
+exports.Step = allureReporterExtensions_1.AllureReporterExtensions.Step;
+exports.addFeature = allureReporterExtensions_1.AllureReporterExtensions.addFeature;
+exports.addStory = allureReporterExtensions_1.AllureReporterExtensions.addStory;
+exports.createAttachment = allureReporterExtensions_1.AllureReporterExtensions.createAttachment;
+exports.Heading = allureReporterExtensions_1.AllureReporterExtensions.Heading;
+exports.ScreenedStep = allureReporterExtensions_1.AllureReporterExtensions.ScreenedStep;
+exports.setScreenshotProvider = allureReporterExtensions_1.AllureReporterExtensions.setScreenshotProvider;
 //# sourceMappingURL=index.js.map
--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,15 @@
-export {AllureReporterExtensions} from "./allureReporterExtensions";
+import {AllureReporterExtensions} from "./allureReporterExtensions";
+
 export {Gherkin} from "./gherkin";
+export const getJasmineAllureReporter = AllureReporterExtensions.getJasmineAllureReporter;
+export const addArgument = AllureReporterExtensions.addArgument;
+export const addDescription = AllureReporterExtensions.addDescription;
+export const addEnvironment = AllureReporterExtensions.addEnvironment;
+export const attachScreenshot = AllureReporterExtensions.attachScreenshot;
+export const Step = AllureReporterExtensions.Step;
+export const addFeature = AllureReporterExtensions.addFeature;
+export const addStory = AllureReporterExtensions.addStory;
+export const createAttachment = AllureReporterExtensions.createAttachment;
+export const Heading = AllureReporterExtensions.Heading;
+export const ScreenedStep = AllureReporterExtensions.ScreenedStep;
+export const setScreenshotProvider = AllureReporterExtensions.setScreenshotProvider;
